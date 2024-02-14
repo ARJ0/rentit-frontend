e5dde52d8f20450f9a3443d72b93e1dd29871e11
ARJ0 <77056816+ARJ0@users.noreply.github.com>
2024-02-15 00:40:31 +0530

    Initialize project

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import Auth from "./Routes/AuthRoute.js";
+import cors from 'cors'
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:3000',
+  methods: ["GET", "PUT", "POST", "DELETE"],
+}))
+const port = process.env.PORT || 5000;
+
+
+
+
+
+
+app.use("/api/auth", Auth);
+
+
+
+
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected With DB Successfull"))
+  .catch((e) => console.log("Db Connection Failed"));
+
+
+const server = app.listen(port, () => {
+  console.log(`Server is Listening on PORT ${port}`);
+})
