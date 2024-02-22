851f788d15fe0e0b1a16d2abeccea4d5c6838b6f
musaddiq25 <90511493+musaddiq25@users.noreply.github.com>
2024-02-22 11:12:53 -0500

    update images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,10 @@ import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturi
 import TravelExploreIcon from '@mui/icons-material/TravelExplore';
 import ChatIcon from '@mui/icons-material/Chat';
 import TourIcon from '@mui/icons-material/Tour';
+import home_banner from '../imges/home_banner.jpg'
+import forklift_1 from '../imges/forklift_1.jpg'
+import heavy_excavator from '../imges/heavy_excavator.jpg'
+import road_roller from '../imges/road_roller.jpg'
 
 function Home() {
   return (
@@ -16,7 +20,7 @@ function Home() {
         exit={{ opacity: 0 }}
         transition={{ duration: .2 }}
       >
-        <div className="top flex-col hero" style={{ backgroundImage: `url(https://img.freepik.com/free-photo/3d-crane-against-sunset-sky_1048-7298.jpg?w=1480&t=st=1708613815~exp=1708614415~hmac=3e72256cd084605c203c148a255627ab8a5c83a998ba530fd13e246c8975092e)` }}> 
+        <div className="top flex-col hero" style={{ backgroundImage: `url(${home_banner})` }}> 
         {/* image source : url - https://www.freepik.com/free-photo/3d-crane-against-sunset-sky_1594716.htm#query=crane%20banner&position=0&from_view=search&track=ais&uuid=274c1463-efb9-4a3e-a6ab-3db9945a4b55 */}
           <h1>
             Get The Goods that You need
@@ -30,14 +34,14 @@ function Home() {
           <div className="why flex">
             <div class="card" style={{"width": "18rem"}}>
               <div class="card-body">
-              <img class="card-img-top" src="https://img.freepik.com/free-photo/logistic-center-with-forklift_23-2148902599.jpg?w=1380&t=st=1708614165~exp=1708614765~hmac=a4f88686f034df5675fc04c90ead6837333ced14cf44dca94577265a57165d04" alt="Card image cap" />
+              <img class="card-img-top" src={forklift_1} alt="Card image cap" />
               {/* image source : url - https://www.freepik.com/free-photo/logistic-center-with-forklift_13291114.htm#query=forklift&position=20&from_view=search&track=sph&uuid=4845b341-667e-456f-87de-3b68c74fc652 */}
                 <p class="card-text mt-5">Forklift</p>
               </div>
             </div>
             <div class="card" style={{"width": "18rem"}}>
               <div class="card-body">
-              <img class="card-img-top" src="https://img.freepik.com/free-photo/heavy-excavator-digging-day-light_23-2149194835.jpg?w=1380&t=st=1708614325~exp=1708614925~hmac=bb6d1d467f538c52c59784f3999f55b15b682efffd5eb008f981bedbb79771fd" alt="Card image cap" />
+              <img class="card-img-top" src={heavy_excavator} alt="Card image cap" />
                 {/* image source : url - https://www.freepik.com/free-photo/heavy-excavator-digging-day-light_20828280.htm#query=jcb&position=28&from_view=search&track=ais&uuid=e21fe950-3584-41e2-8c91-49bb32683260 */}
                 <p class="card-text mt-5">Heavy Excavator</p>
 
@@ -45,7 +49,7 @@ function Home() {
             </div>
             <div class="card" style={{"width": "18rem"}}>
               <div class="card-body">
-              <img class="card-img-top" src="https://img.freepik.com/free-photo/full-shot-man-construction-site_23-2148751969.jpg?w=1380&t=st=1708614466~exp=1708615066~hmac=69bff5aaa55d4c43b265ca6008e219ea52f9faa62e6b5fdba01e126bd4b45e98" alt="Card image cap" />
+              <img class="card-img-top" src={road_roller} alt="Card image cap" />
               {/* image source : url - https://www.freepik.com/free-photo/full-shot-man-construction-site_11106646.htm#page=2&query=Road%20roller&position=16&from_view=search&track=ais&uuid=ecf8c2e7-4485-4ca1-b040-8e482772a51f */}
                 <p class="card-text mt-5">Road Roller</p>
 
