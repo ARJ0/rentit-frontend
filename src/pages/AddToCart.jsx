f3c7a2fa6a6b652e8df1c11aba07c0b495c83d2d
Vishal Chovatiya <129334284+vishalchovatia@users.noreply.github.com>
2024-03-20 01:09:10 +0530

    Bug Fixes

--- src/pages/AddToCart.jsx
+++ src/pages/AddToCart.jsx
@@ -1,205 +1,101 @@
-import React, { useEffect, useState } from "react";
-import SearchIcon from '@mui/icons-material/Search';
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import Select from "react-select";
-import EquipmentCard from "../components/EquipmentCard";
-import { getAllAndSearchEquipment } from "../services/actionCreator";
-import { toaster } from "../services/toaster";
-import { equipmentCategories, equipmentCategoriesTypeName } from "../services/helper";
-import Loader from "../services/Loader";
-import moment from "moment";
-import EquipmentModal from "../components/EquipmentModal";
-// import './test.scss'
+import React, { useEffect, useState } from 'react'
+import SendIcon from '@mui/icons-material/Send';
+import CloseIcon from '@mui/icons-material/Close';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { deleteItemCart, getCart } from '../services/actionCreator';
+import { getlocalStorage } from '../services/helper';
+import Loader from '../services/Loader';
+import RequestModal from '../components/RequestModal';
 
-const sortOp = [
-  {label: "A-Z" , value: 1},
-  {label: "Z-A" , value: 2},
-  {label: "Low to High" , value: 3},
-  {label: "High to Low" , value: 4},
-]
+const AddToCart = ({ closeSidebar }) => {
 
-const Explore = () => {
-  const [equipment, setEquipment] = useState([]);
-  const [category, setCategory] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [cartData, setCartData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [searchTimeout, setSearchTimeout] = useState(setTimeout(() => { }))
-  const [sortBy, setSortBy] = useState()
+  const [selecetedEq, setSelecetedEq] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
-  const [selecetedItem, setSelecetedItem] = useState(null)
 
 
-  const openModal = (info) => {
+  const openModal = (product) => {
     setIsOpen(true);
-    setSelecetedItem(info)
+    setSelecetedEq(product)
   }
   const closeModal = () => {
     setIsOpen(false);
-    setSelecetedItem(null);
-      // get()
-
+    setSelecetedEq(null);
   }
 
-  const getEquipment = (_searchTerm=null) => {
-    setIsLoading(true);
-    let req = {
-      category
-    };
-    if (_searchTerm || searchTerm) {
-      req.keyword =  _searchTerm || searchTerm
-    }
-    if (sortBy) {
-      req.sortBy = [3,4].includes(sortBy.value) ? "rent" : "title";
-      req.sortOrder = [1,3].includes(sortBy.value) ? "asc" : "desc";
-    }
 
-    getAllAndSearchEquipment(req)
-      .then((data) => {
-        setIsLoading(false);
-        setEquipment(data);
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        toaster(error?.response?.data, 'error');
-        console.log("err -=-= ", error);
-      });
-  };
-
-  const changeCategories = (val) => {
-    if (category.indexOf(val) !== -1) {
-      setCategory(category.filter((cat) => cat !== val));
-    } else {
-      setCategory([...category, val]);
+  const _userData = getlocalStorage("loggedUser")
+  const _getCart = () => {
+    const params = {
+      userId: _userData?._id
     }
-  };
-  const _getSearchData = (_searchTerm) => {
-    setIsLoading(true);
-    let req = {
-      category
-    };
-    if (_searchTerm) {
-      req.keyword =  _searchTerm;
-    }
-    if (sortBy) {
-      req.sortBy = [3,4].includes(sortBy.value) ? "rent" : "title";
-      req.sortOrder = [1,3].includes(sortBy.value) ? "asc" : "desc";
-    }
-
-    getAllAndSearchEquipment(req)
-      .then((data) => {
-        setIsLoading(false);
-        setEquipment(data);
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        toaster(error?.response?.data, 'error');
-        console.log("err -=-= ", error);
-      });
+    setIsLoading(true)
+    getCart(params).then((data) => {
+      setCartData(data)
+      setIsLoading(false)
+    }).catch((err) => {
+      setIsLoading(false)
+      console.log("🚀 ~ getCart ~ err:", err)
+    })
   }
+  const removeCartItem = (item) => {
+    const params = {
+      userId: _userData?._id,
+      equipmentId: item?._id
+    }
+    deleteItemCart(params).then((data) => {
+      console.log("data [=-==-= ", data)
+      _getCart()
+    }).catch((err) => {
+      console.log("🚀 ~ deleteItemCart ~ err:", err)
+    })
+  }
+  useEffect(() => {
+    _getCart()
+  }, [])
 
-  const onSearchTextChanged = (e) => {
-    clearTimeout(searchTimeout);
-    const q = e.target.value;
-    setSearchTerm(q);
-    const timeout = setTimeout(() => {
-      _getSearchData(q)
-    }, 500)
-    setSearchTimeout(timeout);
+  return (
+    <div className="position-fixed top-0 z-1031 end-0 h-100 bg-light d-flex flex-column" style={{"width": "45%" }}>
+      <button className="btn  align-self-start m-2 p-1" onClick={closeSidebar}><CloseIcon /></button>
+      <div className="p-3 min-vh-100" style={{"overflow": "auto"}}>
+        {isLoading && <Loader />}
+        <h3><b>Cart</b></h3>
+        {cartData.length === 0 ? (
+          <div className="d-flex flex-column align-items-center justify-content-center" style={{ marginTop: '50%' }}>
+            <p>No products in the cart.</p>
+            <AddShoppingCartIcon style={{ fontSize: 100, color: 'gray' }} />
+          </div>
+        ) : (
+          <ul className="list-group">
+            {cartData.map((product, index) => (
+              <li key={index} className="list-group-item d-flex justify-content-between align-items-end">
+                <div className="d-flex align-items-center">
+                  <img src={product.image} alt={product.title} style={{ width: '140px', marginRight: '10px' }} />
+                  <div>
+                    <h5>{product.title}</h5>
+                    <p>Rent: ${product.rent}</p>
+                  </div>
+                </div>
+                <div>
+                  <div className="d-flex justify-content-between">
+                    <button className="btn btn-primary" style={{ marginRight: '8px' }} onClick={() => {openModal(product) }}>
+                    Borrowing {" "} <SendIcon sx={{ fontSize: 20 }} />
+                    </button>
+                    <button className='btn btn-light' onClick={() => { removeCartItem(product) }}>
+                      Delete {" "}<DeleteForeverIcon sx={{ fontSize: 20 }} />
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      {isOpen && selecetedEq &&  <RequestModal closeModal={closeModal} selecetedEq={selecetedEq}/>}
+    </div>
+  )
 }
 
-  useEffect(() => {
-    getEquipment();
-  }, [category, sortBy]);
-  return (
-    <>
-      <div className="page explore">
-        {isLoading && <Loader />}
-        <div className="top flex-col">
-          <div className="w-100 d-flex align-items-center">
-            <div className="input-group  justify-content-center">
-              <input
-                type="search"
-                className="form-control"
-                placeholder="Search equipment..."
-                value={searchTerm}
-                onChange={onSearchTextChanged}
-              />
-              <span className="input-group-text">
-                <SearchIcon sx={{ fontSize: 20 }} />
-              </span>
-            </div>
-          </div>
-          <div className="categories">
-            <div className={`pill blue filterpill`}>
-              <FilterAltIcon sx={{ fontSize: 30 }} />
-            </div>
-            <div className='pill' style={{"min-width": "10%"}}>
-              <Select 
-                className="select"
-                options={sortOp}
-                onChange={(e) => setSortBy(e)}
-              />
-            </div>
-            {equipmentCategories.map((op) => (
-              <button
-                className={`pill ${category.includes(op.value) ? "blue" : "yellow"}`}
-                onClick={() => {
-                  changeCategories(op.value);
-                }}
-              >
-                {op.label}
-              </button>
-            ))}
-          </div>
-        </div>
-        <div className="container-fluid">
-          <ul>
-            {equipment.length > 0 ?
-              equipment.map((info, index) => {
-                return (
-                  <li className="booking-card text-capitalize" style={{ "background-image": `url(${info.image})` }}>
-                    <div className="book-container">
-                      <div className="content">
-                        <button className="btn" onClick={() => openModal(info)}>Add To Bag</button>
-                      </div>
-                    </div>
-                    <div className="informations-container">
-                      <h2 className="title text-capitalize">{info.title}</h2>
-                      <p className="sub-title">{equipmentCategoriesTypeName[info.category]}</p>
-                      <p className="price"><svg className="icon" style={{ "width": "24px", "height": "24px" }} viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M3,6H21V18H3V6M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M7,8A2,2 0 0,1 5,10V14A2,2 0 0,1 7,16H17A2,2 0 0,1 19,14V10A2,2 0 0,1 17,8H7Z" />
-                      </svg>${info.rent.toFixed(2)} {info.timeperiod}</p>
-                      <div className="more-information">
-                        <div className="info-and-date-container text-capitalize">
-                          {info.unavailableUntil && <div className="box info">
-                            <svg className="icon" style={{ "width": "24px", "height": "24px" }} viewBox="0 0 24 24">
-                              <path fill="currentColor" d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" />
-                            </svg>
-                            <p>{`Sorry, this equipment is currently unavailable. Until ${moment(info.unavailableUntil).format('LL')}`}</p>
-                          </div>}
-                         {!info.unavailableUntil && <div className="box date">
-                            <svg className="icon" style={{ "width": "24px", "height": "24px" }} viewBox="0 0 24 24">
-                              <path fill="currentColor" d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3H18V1M17,12H12V17H17V12Z" />
-                            </svg>
-                            <p>This equipment is available for booking.</p>
-                          </div>}
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                )
-              }) : <div className="flex-col">
-                No Equipment
-              </div>}
-          </ul>
-        </div>
-      </div>
-
-      {isOpen && selecetedItem &&
-        <EquipmentModal closeModal={closeModal} selecetedItem={selecetedItem} />
-      }
-    </>
-  );
-};
-
-export default Explore;
+export default AddToCart
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,73 +1,205 @@
 import React, { useEffect, useState } from "react";
+import SearchIcon from '@mui/icons-material/Search';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import Select from "react-select";
 import EquipmentCard from "../components/EquipmentCard";
-import { getAllEquipment } from "../services/actionCreator";
+import { getAllAndSearchEquipment } from "../services/actionCreator";
 import { toaster } from "../services/toaster";
-import { equipmentCategories } from "../services/helper";
+import { equipmentCategories, equipmentCategoriesTypeName } from "../services/helper";
+import Loader from "../services/Loader";
+import moment from "moment";
+import EquipmentModal from "../components/EquipmentModal";
+// import './test.scss'
 
+const sortOp = [
+  {label: "A-Z" , value: 1},
+  {label: "Z-A" , value: 2},
+  {label: "Low to High" , value: 3},
+  {label: "High to Low" , value: 4},
+]
 
 const Explore = () => {
+  const [equipment, setEquipment] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTimeout, setSearchTimeout] = useState(setTimeout(() => { }))
+  const [sortBy, setSortBy] = useState()
+  const [isOpen, setIsOpen] = useState(false);
+  const [selecetedItem, setSelecetedItem] = useState(null)
 
-  const [equipment, setEquipment] = useState([])
 
-  const getEquipment = () => {
-
-    getAllEquipment().then((data) => {
-      setEquipment(data)
-    }).catch((error) => {
-      toaster(error?.response?.data, 'error');
-      console.log("err -=-= ", error)
-    })
+  const openModal = (info) => {
+    setIsOpen(true);
+    setSelecetedItem(info)
   }
+  const closeModal = () => {
+    setIsOpen(false);
+    setSelecetedItem(null);
+      // get()
+
+  }
+
+  const getEquipment = (_searchTerm=null) => {
+    setIsLoading(true);
+    let req = {
+      category
+    };
+    if (_searchTerm || searchTerm) {
+      req.keyword =  _searchTerm || searchTerm
+    }
+    if (sortBy) {
+      req.sortBy = [3,4].includes(sortBy.value) ? "rent" : "title";
+      req.sortOrder = [1,3].includes(sortBy.value) ? "asc" : "desc";
+    }
+
+    getAllAndSearchEquipment(req)
+      .then((data) => {
+        setIsLoading(false);
+        setEquipment(data);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        toaster(error?.response?.data, 'error');
+        console.log("err -=-= ", error);
+      });
+  };
+
+  const changeCategories = (val) => {
+    if (category.indexOf(val) !== -1) {
+      setCategory(category.filter((cat) => cat !== val));
+    } else {
+      setCategory([...category, val]);
+    }
+  };
+  const _getSearchData = (_searchTerm) => {
+    setIsLoading(true);
+    let req = {
+      category
+    };
+    if (_searchTerm) {
+      req.keyword =  _searchTerm;
+    }
+    if (sortBy) {
+      req.sortBy = [3,4].includes(sortBy.value) ? "rent" : "title";
+      req.sortOrder = [1,3].includes(sortBy.value) ? "asc" : "desc";
+    }
+
+    getAllAndSearchEquipment(req)
+      .then((data) => {
+        setIsLoading(false);
+        setEquipment(data);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        toaster(error?.response?.data, 'error');
+        console.log("err -=-= ", error);
+      });
+  }
+
+  const onSearchTextChanged = (e) => {
+    clearTimeout(searchTimeout);
+    const q = e.target.value;
+    setSearchTerm(q);
+    const timeout = setTimeout(() => {
+      _getSearchData(q)
+    }, 500)
+    setSearchTimeout(timeout);
+}
+
   useEffect(() => {
-    getEquipment()
-  }, [])
-
-
+    getEquipment();
+  }, [category, sortBy]);
   return (
     <>
-      <div
-        className="page explore"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.2 }}
-      >
+      <div className="page explore">
+        {isLoading && <Loader />}
         <div className="top flex-col">
-          <b>Showing Results of : {"All"}</b>
-
+          <div className="w-100 d-flex align-items-center">
+            <div className="input-group  justify-content-center">
+              <input
+                type="search"
+                className="form-control"
+                placeholder="Search equipment..."
+                value={searchTerm}
+                onChange={onSearchTextChanged}
+              />
+              <span className="input-group-text">
+                <SearchIcon sx={{ fontSize: 20 }} />
+              </span>
+            </div>
+          </div>
           <div className="categories">
             <div className={`pill blue filterpill`}>
               <FilterAltIcon sx={{ fontSize: 30 }} />
             </div>
-            <button
-              className={`pill yellow`}
-              onClick={() => {
-              }}
-            >
-              All
-            </button>
+            <div className='pill' style={{"min-width": "10%"}}>
+              <Select 
+                className="select"
+                options={sortOp}
+                onChange={(e) => setSortBy(e)}
+              />
+            </div>
             {equipmentCategories.map((op) => (
               <button
-                className={`pill yellow`}
+                className={`pill ${category.includes(op.value) ? "blue" : "yellow"}`}
+                onClick={() => {
+                  changeCategories(op.value);
+                }}
               >
                 {op.label}
               </button>
             ))}
           </div>
         </div>
-
-        <div className="listProduct">
-          {equipment.length > 0 ? equipment.map((info, index) => {
-            return <EquipmentCard info={info} key={index} />
-          }) :
-            <div className="flex-col">
-              No Equipment
-            </div>}
+        <div className="container-fluid">
+          <ul>
+            {equipment.length > 0 ?
+              equipment.map((info, index) => {
+                return (
+                  <li className="booking-card text-capitalize" style={{ "background-image": `url(${info.image})` }}>
+                    <div className="book-container">
+                      <div className="content">
+                        <button className="btn" onClick={() => openModal(info)}>Add To Bag</button>
+                      </div>
+                    </div>
+                    <div className="informations-container">
+                      <h2 className="title text-capitalize">{info.title}</h2>
+                      <p className="sub-title">{equipmentCategoriesTypeName[info.category]}</p>
+                      <p className="price"><svg className="icon" style={{ "width": "24px", "height": "24px" }} viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M3,6H21V18H3V6M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M7,8A2,2 0 0,1 5,10V14A2,2 0 0,1 7,16H17A2,2 0 0,1 19,14V10A2,2 0 0,1 17,8H7Z" />
+                      </svg>${info.rent.toFixed(2)} {info.timeperiod}</p>
+                      <div className="more-information">
+                        <div className="info-and-date-container text-capitalize">
+                          {info.unavailableUntil && <div className="box info">
+                            <svg className="icon" style={{ "width": "24px", "height": "24px" }} viewBox="0 0 24 24">
+                              <path fill="currentColor" d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" />
+                            </svg>
+                            <p>{`Sorry, this equipment is currently unavailable. Until ${moment(info.unavailableUntil).format('LL')}`}</p>
+                          </div>}
+                         {!info.unavailableUntil && <div className="box date">
+                            <svg className="icon" style={{ "width": "24px", "height": "24px" }} viewBox="0 0 24 24">
+                              <path fill="currentColor" d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3H18V1M17,12H12V17H17V12Z" />
+                            </svg>
+                            <p>This equipment is available for booking.</p>
+                          </div>}
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                )
+              }) : <div className="flex-col">
+                No Equipment
+              </div>}
+          </ul>
         </div>
       </div>
+
+      {isOpen && selecetedItem &&
+        <EquipmentModal closeModal={closeModal} selecetedItem={selecetedItem} />
+      }
     </>
-  )
-}
+  );
+};
 
 export default Explore;
