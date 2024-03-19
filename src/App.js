082bfdc01a936ac7476017cda621dd0c3bb7b513
Vishal Chovatiya <129334284+vishalchovatia@users.noreply.github.com>
2024-03-20 00:19:41 +0530

    Changes like CSS

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import Login from './Login';
 import Registration from './Registration';
 import Explore from "./pages/Explore";
 import './css/index.css'
+import './css/index.scss'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
-import AddEquipment from "./pages/AddEquipment";
 import { YourEquipment } from "./pages/YourEquipment";
+import Request from "./components/Request";
 
 function App() {
   const location = useLocation();
@@ -20,12 +21,12 @@ function App() {
     <ToastContainer />
     <Routes key={location.pathname} location={location}>
       <Route exact path='/' element={<Layout />}> 
-        <Route path="/" element={<Login />} />
+        <Route index element={<Login />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/home" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/add-equipment" element={<AddEquipment/>} />
         <Route path="/your-equipment" element={<YourEquipment/>} />
+        <Route path="/your-request" element={<Request/>} />
       </Route>
     </Routes>
     </div>
