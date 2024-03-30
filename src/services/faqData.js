497558310a0e2673c9253ac5e43524c559e17fd2
ARJ0 <77056816+ARJ0@users.noreply.github.com>
2024-03-30 20:05:57 +0530

    Add FAQ page

---
+++ src/services/faqData.js
@@ -0,0 +1,62 @@
+export const faqData = [
+    {
+        question: 'What is Rentit?',
+        answer: 'Rentit is an innovative web-based platform designed to streamline the industrial machine and heavy vehicle rental process.',
+    },
+    {
+        question: 'How does Rentit work?',
+        answer: 'Rentit simplifies the process of locating heavy machinery and renting it by providing a user-friendly interface with smooth navigation.',
+    },
+    {
+        question: 'What are the key benefits of using Rentit?',
+        answer: 'Rentit offers efficiency, cost savings, and a secure platform for managing and accessing critical assets such as snow removal vehicles, trucks, forklifts, and cranes.',
+    },
+    {
+        question: 'How do I search for specific industrial equipment on Rentit?',
+        answer: 'You can use the search bar on the Rentit platform to search for specific types of industrial machinery such as snow removal vehicles, trucks, forklifts, cranes, etc.You can also use filters to narrow down your searchbased on criteria like location, availability, and rental duration.',
+    },
+    {
+        question: 'What are the payment options available for renting industrial equipment?',
+        answer: 'Rentit offers various payment options including credit/debit card payments, online bank transfers, and payment gateways.You can choose the most convenient payment method during the checkout process.',
+    },
+    {
+        question: 'Can I extend the rental duration of equipment if needed?',
+        answer: 'Yes, Rentit allows users to extend the rental duration of equipment if needed, subject to availability.You can request an extension through your Rentit account or by contacting customer support.',
+    },
+    {
+        question: 'What happens if the rented equipment is damaged or lost?',
+        answer: 'In case of damage or loss of rented equipment, please notify Rentit immediately.Depending on the situation, you may be liable for repair costs or replacement fees as per the rental agreement terms.',
+    },
+    {
+        question: 'Are there any discounts or promotions available for frequent renters?',
+        answer: 'Rentit offers discounts and promotions for frequent renters and bulk orders.Keep an eye on our promotions page or subscribe to our newsletter to stay updated on the latest deals and offers.',
+    },
+    {
+        question: 'Can I rent equipment for short-term projects?',
+        answer: 'Yes, Rentit caters to both short-term and long-term equipment rental needs.Whether you need equipment for a day, a week, a month, or longer, you can find flexible rental options on our platform.',
+    },
+    {
+        question: 'How do I return the rented equipment at the end of my rental period?',
+        answer: 'When your rental period ends, you can arrange for equipment pickup or return it to the designated drop- off location as specified in your rental agreement.Make sure the equipment is cleaned, inspected, and ready for return as per the rental guidelines.',
+    },
+    {
+        question: 'Is Rentit available for both businesses and individuals?',
+        answer: 'Yes, Rentit caters to both businesses and individuals looking to rent industrial equipment.Whether you\'re a construction company, logistics firm, or individual contractor, Rentit provides rental solutions to meet your needs.',
+    },
+    {
+        question: 'Can I schedule equipment pickups and returns in advance?',
+        answer: 'Absolutely, Rentit allows users to schedule equipment pickups and returns in advance to ensure smooth logistics and availability.You can specify preferred dates and times during the rental booking process.',
+    },
+    {
+        question: 'What types of industrial equipment are available for rent on Rentit ? ',
+        answer: 'Rentit offers a wide range of industrial equipment for rent, including but not limited to trucks, forklifts, cranes, excavators, loaders, bulldozers, and more.You can explore our inventory online or through the Rentit app.',
+    },
+    {
+        question: 'How does Rentit ensure the quality and safety of rented equipment ? ',
+        answer: 'Rentit conducts regular inspections and maintenance checks on rented equipment to ensure quality and safety standards are met.We work with reputable suppliers and adhere to industry regulations to provide reliable and safe equipment rentals.',
+    },
+    {
+        question: 'Can I get assistance or technical support during my equipment rental ? ',
+        answer: 'Yes, Rentit provides customer support and technical assistance throughout your equipment rental period.Our support team is available to address any inquiries, concerns, or technical issues you may encounter.',
+    },
+];
